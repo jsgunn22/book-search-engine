d197d9781a729f20c75ae1ddf821a461aadcbd3f
Jeffrey Gunn <j.s.gunn22@gmail.com>
2023-11-04 10:06:54 -0700

    sets up save book

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,11 +11,11 @@ export const QUERY_USER = gql`
 `;
 
 export const QUERY_SINGLE_USER = gql`
-    query singleUser($userId: ID!) {
-        user(userIdL $userId) {
-           _id
-           username
-           email
-        }
+  query singleUser($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      username
+      email
     }
+  }
 `;
